40742a1a24dd225ba3c60bd1b3600d7c5b97ac98
Fabio Aiex Alves <fabio.aiex@hotmail.com>
2022-10-06 13:38:06 -0300

    Criando paginas de estilo.

---
+++ vite-project/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import { 
+    Container,
+    Content,
+    ImageLogo,
+ } from './styles';
+
+export function Header() {
+    return(
+        <Container>
+            <Content>
+                <ImageLogo />
+                <button>
+                    
+                </button>
+            </Content>
+        </Container>
+    )
+}
